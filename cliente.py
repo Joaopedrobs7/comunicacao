28a58f390b027e8f902d94c0ddb1a0d3a9c644ff
João Pedro Batista da silva <116274892+Joaopedrobs7@users.noreply.github.com>
2024-04-21 17:31:21 -0300

    Update cliente.py
    get price on client side

--- cliente.py
+++ cliente.py
@@ -6,12 +6,12 @@ def generate_checksum(data):
     return hashlib.md5(data.encode()).hexdigest()
 
 host = gethostname()
-port = 5050
+port = 5052
 addr = (host, port)
 format = 'utf-8'
 
 client = socket(AF_INET, SOCK_STREAM)
-client.settimeout(5)  # setting a timeout of 5 seconds
+client.settimeout(10)  # setting a timeout of 5 seconds
 client.connect(addr)
 
 sequence_number = 0
@@ -23,12 +23,22 @@ while True:
     checksum = generate_checksum(msg)
     message = f"{sequence_number};{msg};{checksum}"
     client.send(message.encode(format))
+    
+    
+
+
+    
 
     # Wait for server ACK
     try:
         server_msg = client.recv(1024).decode(format)
         if server_msg == f"ACK{sequence_number}":
             print("Message received by server.")
+            
+            server_msg = client.recv(1024)
+            server_msg.decode(format)
+            print(server_msg)
+        
         elif server_msg == f"NACK{sequence_number}":
             print("Transmission error, resending...")
             continue
